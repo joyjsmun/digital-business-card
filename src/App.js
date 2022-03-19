3d953806608115d57accd5a3d7c03f075c16cf73
joyjsmun <joy.js.mun@gmail.com>
2022-03-18 22:07:41 -0700

    added img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import styled from "styled-components";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+
 function App() {
   return (
-    <>
+    <Wrapper>
     <Header />
     <Main />
     <Footer />
-    </>
+    </Wrapper>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import styled from "styled-components";
+//import photo from "../images/joy.png";
+
+
+const Main = styled.div`
+    width: 400px;
+    height: 400px;
+    
+
+`
+
+const Img = styled.img`
+
+  
+`
 
 function Header() {
     return (
-        <main>
-            <img src="" />
-        </main>
+        <Main>
+            <Img src={require('../images/joy.png')} />
+        </Main>
     )
 }
 
