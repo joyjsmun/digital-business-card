012b94082fd2e8aab78bde239e2f336a0ba1de76
joyjsmun <joy.js.mun@gmail.com>
2022-03-19 23:58:43 -0700

    added styling to main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
+const Main = styled.div``
+
+const Name = styled.h1``
+
+const Title = styled.h3``
+
+const Field = styled.h5``
+
+const Buttons = styled.ul``
+
+const Email = styled.li``
+
+const Linkedin = styled.li``
+
+const About = styled.div``
+
+const AboutTitle = styled.h3``
+
+const Interest = styled.div``
+
+const InterestTitle = styled.h3``
+
+
 function Main() {
     return (
         <Main>
